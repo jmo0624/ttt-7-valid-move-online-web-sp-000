fa60dd7fab0bf809b97096f94e9b9240e021bfb1
Josh Morris <jmo0624@gmail.com>
2019-04-18 00:43:40 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -28,7 +28,7 @@ def valid_move?(board, index)
     
   end
   
-  if (position_taken?(array, ind)) == false && (on_board?(num)) == true
+  if (position_taken?(array, index)) == false && (on_board?(index)) == true
   
 end
 
